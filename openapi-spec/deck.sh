15be66470b03ae410fd37032c107b04f5f04d10e
Sagar Mhatre <sagar.mhatre@konghq.com>
2025-10-25 11:28:02 +0800

    syncing to kong & Testing via curl

--- openapi-spec/deck.sh
+++ openapi-spec/deck.sh
@@ -1,3 +1,8 @@
 cd /Users/sagar.mhatre/Documents/src/
 cd fastapi-httpbin/openapi-spec
 deck file openapi2kong  -s openapi.yaml -o kong.yaml
+
+
+deck gateway ping --kong-addr http://localhost:8001
+deck gateway diff kong.yaml -w openapi
+deck gateway sync kong.yaml -w openapi
